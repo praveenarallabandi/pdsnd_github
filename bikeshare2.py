3a10eea93be749f28739d0515895d2b3ad4b4c95
PraveenaRallabandi <praveenarallabandi@gmail.com>
2018-12-02 22:44:22 -0500

    work in progreess add popularhours, trip and stationd

--- bikeshare2.py
+++ bikeshare2.py
@@ -2,6 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 import collections
+from datetime import datetime
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
@@ -12,6 +13,7 @@ class BikeShare:
         self.city = city
         self.month = month
         self.day = day
+        self.df = None
             
     def load_data(self):
         """
@@ -24,8 +26,27 @@ class BikeShare:
         Returns:
             df - Pandas DataFrame containing city data filtered by month and day
         """
-        print('Printing city' + self.city)
-        # return df // TODO
+        print('Printing city ' + self.city)
+        print('Printing month ' + self.month)
+        print('Printing day ' + self.day)
+
+        headers = ['id', 'start_time', 'end_time', 'trip_duration', 'start_station', 'end_station', 'user_type', 'gender', 'birth_year']
+        dtypes = {'id': 'str', 'start_time': 'str', 'end_time': 'str', 'trip_duration': 'str', 'start_station': 'str', 'end_station': 'str', 'user_type': 'str', 'gender': 'str', 'birth_year': 'str'}
+        dateCols = ['start_time', 'end_time']
+        
+        if self.city == 'chicago':
+            self.df = pd.read_csv("./chicago.csv", sep=',', header=None, names=headers, dtype=dtypes, parse_dates=dateCols)
+            self.df['start_time'] = pd.to_datetime(self.df['start_time'], errors='coerce')
+            print(self.df.head(5))
+
+        if self.city == 'washington':
+            self.df = pd.read_csv("./washington.csv")
+            print(self.df.head(5))
+
+        if self.city == 'new york':
+            self.df = pd.read_csv("./new_york_city.csv")
+            print(self.df.head(5))
+        # return df // TODO 
 
     def time_stats(self, df):
         """Displays statistics on the most frequent times of travel."""
@@ -34,57 +55,53 @@ class BikeShare:
         start_time = time.time()
 
         # TO DO: display the most common month
-    if time == 'month':
+        if time == 'month':
             months = ['january', 'february', 'march', 'april', 'may', 'june']
             month = months.index(month) + 1
             df = df[df['month'] == month]
 
         # TO DO: display the most common day of week
-    if time == 'day_of_week':
-            days = ['Monday', 'Tuesday', 
-            'Wednesday', 'Thursday', 
-            'Friday', 'Saturday', 'Sunday']
+        if time == 'day_of_week':
+            days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
             for d in days:
                 if day_of_week.capitalize() in d:
                     day_of_week = d
-            df = df[df['day_of_week'] == day_of_week]
-        # TO DO: display the most common start hour
-
-    def popular_hour(self, df):
+                df = df[df['day_of_week'] == day_of_week]
         
-            most_pop_hour = int(df['start_time'].dt.hour.mode())
-            if most_pop_hour == 0:
-                am_pm = 'am'
-                pop_hour_readable = 12
-            elif 1 <= most_pop_hour < 13:
-                am_pm = 'am'
-                pop_hour_readable = most_pop_hour
-            elif 13 <= most_pop_hour < 24:
-                am_pm = 'pm'
-                pop_hour_readable = most_pop_hour - 12
-                print('The most popular hour of day for start time is {}{},'.format(pop_hour_readable, am_pm))
+    # TO DO: display the most common start hour
+    def popular_hour(self):
+        print(self.df['start_time'].dt.hour.mode())
+        most_pop_hour = int(self.df['start_time'].dt.hour.mode())
+        if most_pop_hour == 0:
+            am_pm = 'am'
+            pop_hour_readable = 12
+        elif 1 <= most_pop_hour < 13:
+            am_pm = 'am'
+            pop_hour_readable = most_pop_hour
+        elif 13 <= most_pop_hour < 24:
+            am_pm = 'pm'
+            pop_hour_readable = most_pop_hour - 12
+            print('The most popular hour of day for start time is {}{},'.format(pop_hour_readable, am_pm))
 
 
-        # TO DO: display most commonly used start station
-    def popular_stations(self, df):
-        pop_start = df['start_station'].mode().to_string(index = False)
+    # TO DO: display most commonly used start station
+    def popular_stations(self):
+        pop_start = self.df['start_station'].mode().to_string(index = False)
         print('The most popular start station is {}.'.format(pop_start))
         
-        # TO DO: display most commonly used end station
-
-    def popular_trip(self, df):
-        most_pop_trip = df['journey'].mode().to_string(index = False)
+    # TO DO: display most commonly used end station
+    def popular_trip(self):
+        most_pop_trip = self.df['end_station'].mode().to_string(index = False)
         print('The most popular trip is {}.'.format(most_pop_trip))
 
 
-        # TO DO: display total travel time
-
-    def total_trvel_time(self, df):
-        print("That took %s seconds." % (time.time() - start_time))
+    # TO DO: display total travel time
+    def total_trvel_time(self):
+        print("That took %s seconds." % (time.time() - self.df['start_time']))
         print("\nCalculating the next statistic...")
-        start_time = time.time()
-        # TO DO: display mean travel time
-
+        self.df['start_time'] = time.time()
+        
+    # TO DO: display mean travel time
     def mean_trip_duration(self, df):
         print("That took %s seconds." % (time.time() - start_time))
         print("\nCalculating the next statistic...")
@@ -100,27 +117,24 @@ class BikeShare:
         print('\nCalculating User Stats...\n')
         start_time = time.time()
 
-        # TO DO: Display counts of user types
-
-    def users(self, df):
-    
-        subs = df.query('user_type == "Subscriber"').user_type.count()
-        cust = df.query('user_type == "Customer"').user_type.count()
+    # TO DO: Display counts of user types
+    def users(self):
+        subs = self.df.query('user_type == "Subscriber"').user_type.count()
+        cust = self.df.query('user_type == "Customer"').user_type.count()
         print('There are {} Subscribers and {} Customers.'.format(subs, cust))
 
-        # TO DO: Display counts of gender
-    def gender(self, df):
-        male_count = df.query('gender == "Male"').gender.count()
-        female_count = df.query('gender == "Male"').gender.count()
+    # TO DO: Display counts of gender
+    def gender(self):
+        male_count = self.df.query('gender == "Male"').gender.count()
+        female_count = self.df.query('gender == "Male"').gender.count()
         print('There are {} male users and {} female users.'.format(male_count, female_count))
 
 
-        # TO DO: Display earliest, most recent, and most common year of birth
-    def birth_years(self, df):
-        
-        earliest = int(df['birth_year'].min())
-        latest = int(df['birth_year'].max())
-        mode = int(df['birth_year'].mode())
+    # TO DO: Display earliest, most recent, and most common year of birth
+    def birth_years(self):
+        earliest = int(self.df['birth_year'].min())
+        latest = int(self.df['birth_year'].max())
+        mode = int(self.df['birth_year'].mode())
         print('The oldest users are born in {}.\nThe youngest users are born in {}.'
             '\nThe most popular birth year is {}.'.format(earliest, latest, mode))
 
@@ -158,6 +172,13 @@ def get_filters():
 
     bikeShare = BikeShare(city, month, day)
     bikeShare.load_data()
+    bikeShare.popular_hour()
+    bikeShare.popular_stations()
+    bikeShare.popular_trip()
+    # bikeShare.total_trvel_time() // TODO
+    bikeShare.users()
+    bikeShare.gender()
+    # bikeShare.birth_years() //TODO
 
 def main():
     get_filters()
